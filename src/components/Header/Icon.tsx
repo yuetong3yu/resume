40946b592bea788e35f98513b75ea8aa316a31c4
yuetong3yu <yuetong3yu@gmail.com>
2022-05-29 12:20:33 +0800

    wip

---
+++ src/components/Header/Icon.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Icon: React.FC = () => {
+  return <div className=" w-8 h-8 rounded-md bg-red-300">Yu</div>
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Icon } from './Icon'
+
+export const Header: React.FC = () => {
+  return (
+    <div className=" h-16 bg-slate-300">
+      <Icon />
+    </div>
+  )
+}
