3d7c468a8e4782b6370f6babba4dc517aa7676fe
yuetong3yu <yuetong3yu@gmail.com>
2022-12-13 10:00:00 +0800

    update styles

--- src/components/ContactInfo.vue
+++ src/components/ContactInfo.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
+  <div class="contact-info-container">
     <h1 class="name">Chris Yu</h1>
     <div class="contact-info">
       <span>+86 13438128255</span>
-      <span> | </span>
+      <span class="divider"></span>
       <span>yuetong3yu@gmail.com</span>
-      <span> | </span>
+      <span class="divider"></span>
       <span>Auckland, NZ</span>
     </div>
     <div class="brief-intro">
@@ -14,3 +14,28 @@
     </div>
   </div>
 </template>
+
+<style scoped>
+.contact-info-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1mm;
+}
+.name {
+  font-size: 1.5em;
+  font-weight: bold;
+  text-align: center;
+}
+.contact-info {
+  display: flex;
+  align-items: center;
+}
+.divider {
+  display: inline-block;
+  width: 0.5mm;
+  height: 6mm;
+  margin: 0 2mm;
+  background-color: black;
+}
+</style>
