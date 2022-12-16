69c2e36f253253bf52e5ec282502dca093c91aeb
yuetong3yu <yuetong3yu@gmail.com>
2022-12-16 10:00:00 +0800

    Add Summary

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import ContactInfo from './components/ContactInfo.vue'
 import Skills from './components/Skills.vue'
+import Summary from './components/Summary.vue'
 </script>
 
 <template>
   <div class="a4-page">
     <contact-info></contact-info>
+    <summary></summary>
     <skills></skills>
   </div>
 </template>
---
+++ src/components/Summary.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="section-container">!!!!!!</div>
+</template>
