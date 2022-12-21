d81c8f4ae9e2fba9cdc3f7bc18ad5c1a4b02b2a8
yuetong3yu <yuetong3yu@gmail.com>
2022-12-21 11:19:17 +0800

    Add Skills

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="section-container">Skills</div>
+  <div class="section-content">
+    <ul>
+      <li>Programming Languages: JavaScript, TypeScript</li>
+      <li>
+        Frameworks and Libraries: React, Vue, Angular, Sass, TailwindCSS,
+        Webpack, Slate.js(for rich editor)
+      </li>
+      <li>Technologies: Git, Alibaba Cloud Service, AWS</li>
+      <li>Tools: VSCode, MacOS</li>
+    </ul>
+  </div>
 </template>
 
 <style scoped></style>
